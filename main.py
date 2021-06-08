9787fe25ead5970ed044615e9aaa4d45960132f0
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-06-08 16:37:14 +0200

    Update

--- main.py
+++ main.py
@@ -11,54 +11,39 @@ root.config(bg="#C2EFEB")
 
 value = IntVar()
 
+# Label for currency code
+label_curr = Label(root, text="Your Currency Code", font=("Garuda", 15))
+label_curr.config(bg="#ECFEE8")
+label_curr.pack()
 
-# Retrieving the information from an external JSON file as a source of reference
-information = requests.get('https://v6.exchangerate-api.com/v6/89dcd9e8cc7777ded2575ce1/latest/USD')
-information_json = information.json()
+# Entry for currency code
+entry_curr = Entry(root, text="Currency Code", font=("Garuda", 15))
+entry_curr.pack()
 
-conversion_rate = information_json['conversion_rates']
-# print(conversion_rate)
+# Label for currency value
+label_val = Label(root, text="Your Currency Value", font=("Garuda", 15))
+label_val.config(bg="#ECFEE8")
+label_val.pack()
 
-
-# Creating a label and entry for the results
-value_label = Label(root, text="Value", font=("Arial", 20))
-value_label.config(bg="#ECFEE8")
-value_label.pack()
-
-value_entry = Entry(root, textvariable=value, width=40)
-value_entry.config(bg="#ECFEE8")
-value_entry.pack()
-
-
-# Creating the FROM (Standard value is USD)
-from_label = Label(root, text="From: USD", font=("Arial", 20))
-from_label.config(bg="#ECFEE8")
-from_label.pack()
-
-
-# Doing the Conversion of the data with its loop
-convert = Label(root, text="To:", font=("Arial", 20))
-convert.config(bg="#ECFEE8")
-convert.pack()
-
-convert_list = Listbox(root, width=20)
-for i in conversion_rate.keys():
-    convert_list.insert(END, str(i))
-convert_list.pack()
-
-convert_label = Label(root, text="Converted to: ", font=("Arial", 20))
-convert_label.config(bg="#ECFEE8")
-convert_label.pack()
+# Entry for currency value
+entry_val = Entry(root, text="Value", font=("Garuda", 15))
+entry_val.pack()
 
 
 def convert_curr():
-    num = float(value_entry.get())
-    print(information_json['conversion_rates'][convert_list.get(ACTIVE)])
-    ans = num * information_json['conversion_rates'][convert_list.get(ACTIVE)]
-    convert_label['text'] = ans
+    url = "https://v6.exchangerate-api.com/v6/89dcd9e8cc7777ded2575ce1/latest/" + entry_curr.get()
+    information = requests.get(url).json()
+    output = int(entry_val.get()) * information["conversion_rates"]["USD"]
+    output_val.config(text=output)
 
 
-convert_btn = Button(root, command=convert_curr, text="Convert", font=("Arial", 20), width=20)
+# Output value
+output_val = Label(root, text="Output", font=("Garuda", 15))
+output_val.config(bg="#ECFEE8")
+output_val.pack()
+
+
+convert_btn = Button(root, command=convert_curr, text="Convert", font=("Garuda", 15), width=10)
 convert_btn.config(bg="#ECFEE8")
 convert_btn.pack()
 
